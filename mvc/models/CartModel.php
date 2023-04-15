ff558e331a0c01419bebc67afe9dd2af6bea1b51
Dylan <dylannguyen2207@gmail.com>
2023-04-15 22:24:07 +0700

    hehehe

--- mvc/models/CartModel.php
+++ mvc/models/CartModel.php
@@ -2,10 +2,51 @@
 class CartModel extends DB
 {
 
+    public function showCart()
+    {
+        foreach ($_SESSION['cart'] as $item) {
+            $masp = $item[0][0];
+            $hinhanh = $item[0][1];
+            $tensp = $item[0][2];
+            $dongia = $item[0][3];
+            $soluong = $item[0][4];
+            $thanhtien = $item[0][5];
+            $matk = $_SESSION['user'];
+            mysqli_query($this->con, "INSERT INTO `giohang`(`MaSP`, `hinhanh`, `TenSP`, `DonGia`, `SoLuong`, `thanhTien`, `MaTK`) VALUES ($masp,'$hinhanh','$tensp',$dongia,$soluong,$thanhtien,$matk)");
+        }
+        unset($_SESSION['cart']);
+    }
+
+    public function getCart()
+    {
+        if (isset($_SESSION['user'])) {
+            $matk = $_SESSION['user'];
+            $result = mysqli_query($this->con, "SELECT * FROM giohang WHERE MaTK = 60");
+            return $result;
+        }
+    }
+
     public function addToCart($data)
     {
         if (isset($_SESSION["user"])) {
-            echo "da dang nhap"; // kiểm tra xem người dùng có lưu sản phẩm nào trong giỏ hàng (session) chưa, nếu có lưu lại vào csdl trước r mới lưu sản phẩm cần thêm hiện tại vào csdl sau
+            echo "da dang nhap";
+            $result = mysqli_fetch_array(mysqli_query($this->con, "SELECT * FROM sanpham WHERE masp = $data"));
+            $masp = $result[0];
+            $hinhanh = $result[4];
+            $tensp = $result[1];
+            $dongia = $result[2];
+            $soluong = "1";
+            $thanhtien = $dongia;
+            $matk = $_SESSION['user'];
+
+            $check = mysqli_fetch_array(mysqli_query($this->con, "SELECT * FROM giohang WHERE masp = $data AND MaTK = $matk"));
+            $soluongmoi = $check[5]+1;
+            if($check){
+                mysqli_query($this->con, "UPDATE `giohang` SET `SoLuong`='$soluongmoi' WHERE masp = $data AND MaTK = $matk");
+            }
+            else{
+                mysqli_query($this->con, "INSERT INTO `giohang`(`MaSP`, `hinhanh`, `TenSP`, `DonGia`, `SoLuong`, `thanhTien`, `MaTK`) VALUES ($masp,'$hinhanh','$tensp',$dongia,$soluong,$thanhtien,$matk)");
+            }
         } else {
             $result = mysqli_fetch_array(mysqli_query($this->con, "SELECT * FROM sanpham WHERE masp = '$data'"));
             // echo "<pre>";
@@ -14,23 +55,24 @@ class CartModel extends DB
                 $result[0], //masp
                 $result[4], //hinh anh san pham
                 $result[1], // ten san pham
-                $result[2], // gia san pham
+                $result[2],
+                // gia san pham
                 "1", //so luong = 1
                 $result[2] // thanh tien
             ];
 
-            if (isset($_SESSION['cart'][$data])){
+            if (isset($_SESSION['cart'][$data])) {
                 $productInfo = [
                     $result[0], //masp
                     $result[4], //hinh anh san pham
                     $result[1], // ten san pham
                     $result[2], // gia san pham
                     $_SESSION['cart'][$data][0][4] + 1, //so luong + 1
-                    $result[2]*$_SESSION['cart'][$data][0][4] // thanh tien
+                    $result[2] * $_SESSION['cart'][$data][0][4] // thanh tien
                 ];
 
                 echo "<pre>";
-                print_r($result[2]*$_SESSION['cart'][$data][0][4]);
+                print_r($result[2] * $_SESSION['cart'][$data][0][4]);
                 $_SESSION['cart'][$data] = [$productInfo];
 
             } else {
--- mvc/views/cart.php
+++ mvc/views/cart.php
@@ -39,6 +39,7 @@
               </thead>
               <tbody>
                 <?php
+                $total = 0;
                 if (isset($_SESSION['cart'])) {
                   foreach ($_SESSION['cart'] as $item) {
                     ?>
@@ -53,19 +54,48 @@
                       </td>
                       <td><input style="width: 45px" value="<?php echo $item[0][4] ?>" type="number"></td>
                       <td>
-                        <?php echo $item[0][4]*$item[0][3] ?> đ
+                        <?php echo $item[0][4] * $item[0][3] ?> đ
                       </td>
                       <td><a href="../cart/deleteCartItem/<?php echo $item[0][0] ?>" class="btn btn-sm btn-danger">Xóa</a>
                       </td>
                       <td>
                         <form method="post" action="../vnpay_php/vnpay_create_payment.php">
                           <input type="hidden" name="idP" id="" value="<?php echo $item[0][0] ?>">
-                          <input type="hidden" name="total" id="" value="<?php echo $item[0][4]*$item[0][3] ?>">
+                          <input type="hidden" name="total" id="" value="<?php echo $item[0][4] * $item[0][3] ?>">
                           <button type="submit" class="btn btn-sm btn-primary">Đặt hàng</button>
                         </form>
                       </td>
                     </tr>
                     <?php
+                  }
+                } else if (isset($_SESSION['user'])) {
+                    while($result = mysqli_fetch_array($data)){
+                    ?>
+                      <tr>
+                      <th class="w-25" scope="row"><img class="w-25" src="../public/product_imgs/<?php echo $result[2] ?>"
+                          alt=""></th>
+                      <td>
+                        <?php echo $result[3] ?>
+                      </td>
+                      <td>
+                        <?php echo $result[4] ?> đ
+                      </td>
+                      <td><input style="width: 45px" value="<?php echo $result[5] ?>" type="number"></td>
+                      <td>
+                        <?php echo $result[4]*$result[5] ?> đ
+                      </td>
+                      <td><a href="../cart/deleteCartItem/<?php echo $result ?>" class="btn btn-sm btn-danger">Xóa</a>
+                      </td>
+                      <td>
+                        <form method="post" action="../vnpay_php/vnpay_create_payment.php">
+                          <input type="hidden" name="idP" id="" value="<?php echo $result ?>">
+                          <input type="hidden" name="total" id="" value="<?php echo $result[4] * $result[5 ] ?>">
+                          <button type="submit" class="btn btn-sm btn-primary">Đặt hàng</button>
+                        </form>
+                      </td>
+                    </tr>
+                    <?php
+                    $total += $result[4]*$result[5];
                   }
                 }
                 ?>
@@ -83,25 +113,28 @@
             <h5 class="card-title">Tổng tiền</h5>
             <p class="card-text">Tổng tiền:
               <?php
-              if ($data != null) {
+              if ($data != null && $total == 0) {
                 echo $data . "đ";
-              } else {
+              } 
+              else if(isset($total) && $total != null){
+                echo $total  . " đ";
+              }
+              else {
                 echo "0 đ";
               }
 
               ?>
-            </p>
             <form method="post" action="../vnpay_php/vnpay_create_payment.php">
               <input type="hidden" name="payAll">
               <input type="hidden" name="total" id="" value="<?php echo $data ?>">
               <button class="btn btn-primary">Thanh toán</button>
             </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <!-- Nạp các tệp JavaScript của Bootstrap 5 -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Nạp các tệp JavaScript của Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
